ff458a576fbd587af8b2938881e5a66761e33a36
amir <zeroday1092@gmail.com>
2021-08-19 23:45:22 +0430

    added django file to git

---
+++ blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+from extensions.utils import jalali_converter
+
+# Create your models here.
+class Article(models.Model):
+	STATUS_CHOICES = (
+		('d', 'پیش نویس'),
+		('p', 'منتشر شده'),
+	)
+	title = models.CharField(max_length=200, verbose_name="عنوان مقاله")
+	slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
+	description = models.TextField(verbose_name="محتوا")
+	thumbnail = models.ImageField(upload_to="image", verbose_name="تصویر مقاله")
+	publish = models.DateTimeField(default=timezone.now, verbose_name="زمان انتشار")
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=1, choices= STATUS_CHOICES, verbose_name="وضعیت")
+
+	class Meta:
+		verbose_name = "مقاله"
+		verbose_name_plural = "مقالات"
+
+	def __str__(self):
+		return self.title
+
+	def jpublish(self):
+		return jalali_converter(self.publish)
+
+	jpublish.short_description = "زمان انتشار"
